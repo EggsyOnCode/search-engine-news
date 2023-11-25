d25d1a808cc66ce5fb1ebeed6901f6bd726252b9
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-26 03:03:55 +0500

    updated folder structure

--- tokenizer.py
+++ tokenizer.py
@@ -3,41 +3,48 @@ import codecs
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import string
-
-# Download NLTK stopwords if not already downloaded
 import nltk
+
 nltk.download('stopwords')
 
+# test data fro testing the script is located in /test_data
+
 # Read the JSON file
-with open('test.json', 'r', encoding='utf-8') as file:
-    json_data = json.load(file)
+with open('./test_data/abcnews.json', 'r', encoding='utf-8') as file:
+    json_data_array = json.load(file)
 
-# Extract necessary fields
-content = json_data.get('content', '')
+new_data = []
 
-# Decode escape sequences in the 'content' field
-content = codecs.decode(content, 'unicode_escape')
+# Process each JSON object in the array
+for json_data in json_data_array:
+    # Extract necessary fields
+    content = json_data.get('content', '')
 
-# Tokenize content into words using NLTK and remove punctuation
-word_list = word_tokenize(content)
-word_list = [word for word in word_list if word.isalnum()]  # Remove punctuation
+    # Decode escape sequences in the 'content' field
+    content = codecs.decode(content, 'unicode_escape')
 
-# Remove stopwords
-stop_words = set(stopwords.words('english'))
-word_list = [word for word in word_list if word.lower() not in stop_words]
+    # Tokenize content into words using NLTK and remove punctuation
+    word_list = word_tokenize(content)
+    word_list = [word for word in word_list if word.isalnum()]  # Remove punctuation
 
-# Create a new JSON object
-new_json = {
-    'field': json_data.get('id', ''),
-    'date': json_data.get('date', ''),
-    'source': json_data.get('source', ''),
-    'title': json_data.get('title', ''),
-    'word_list': word_list,
-    'url': json_data.get('url', ''),
-    'author': json_data.get('author', ''),
-    'publication_date': json_data.get('published', '')
-}
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    word_list = [word for word in word_list if word.lower() not in stop_words]
 
-# Write the new JSON object to a file
-with open('new_data2.json', 'w', encoding='utf-8') as new_file:
-    json.dump(new_json, new_file, ensure_ascii=False, indent=2)
+    # Create a new JSON object
+    new_json = {
+        'field': json_data.get('id', ''),
+        'date': json_data.get('date', ''),
+        'source': json_data.get('source', ''),
+        'title': json_data.get('title', ''),
+        'word_list': word_list,
+        'url': json_data.get('url', ''),
+        'author': json_data.get('author', ''),
+        'publication_date': json_data.get('published', '')
+    }
+
+    new_data.append(new_json)
+
+# Write the new JSON objects to a file
+with open('./test_data/new_data2.json', 'w', encoding='utf-8') as new_file:
+    json.dump(new_data, new_file, ensure_ascii=False, indent=2)

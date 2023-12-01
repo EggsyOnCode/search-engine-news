184b2d1b1c566a949a5bb72662bef5c1f453d957
EggsyOnCode <farzansaqib2003@gmail.com>
2023-12-01 18:12:14 +0500

    added potential scripts for forward index

---
+++ indexing/forwardIndex.py
@@ -0,0 +1,108 @@
+import hashlib
+
+# Node structure for M-ary tree (Trie)
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class ForwardIndex:
+    def __init__(self):
+        self.index = {}
+
+    def hash_document(self, title):
+        # Generate a hash value for the document title
+        return hashlib.md5(title.encode()).hexdigest()
+
+    def insert_word_list(self, hash_value, word_list):
+        # Insert word list into an M-ary tree (Trie)
+        root = TrieNode()
+        for word in word_list:
+                current = root
+                for char in word:
+                    if char not in current.children:
+                        current.children[char] = TrieNode()
+                    current = current.children[char]
+                current.is_end_of_word = True
+
+        self.index[hash_value] = root  # Map hash to the M-ary tree
+
+    # these two func are for getting the word_lsit associated with a document
+    def get_word_list(self, title):
+        # Retrieve word list for a specific document
+        hash_value = self.hash_document(title)
+        if hash_value in self.index:
+            root_node = self.index[hash_value]
+            word_list = []
+            self.construct_word_list(root_node, "", word_list)
+            return word_list  # Return the list of words associated with the document
+        else:
+            return None  # Document not found
+
+    def construct_word_list(self, node, current_word, word_list):
+        # Traverse the Trie structure to construct the word list
+        if node.is_end_of_word:
+            word_list.append(current_word)
+
+        for char, child_node in node.children.items():
+            self.construct_word_list(child_node, current_word + char, word_list)
+            
+    # func to retireve the word freq of a word in a doc
+    def count_word_frequency(self, title, word):
+        # Count the frequency of a specific word in a document
+        word_list = self.get_word_list(title)
+        print(word_list)
+        if word_list:
+            return word_list.count(word)
+        else:
+            return 0  # Document not found or no word list
+
+    def does_word_exist(self, title, word):
+        # Check if a specific word exists in a document
+        word_list = self.get_word_list(title)
+        if word_list:
+            return word in word_list
+        else:
+            return False  # Document not found or no word list
+
+    def print_node_contents(self, node):
+        # Print the content at each node in the M-ary tree
+        if node:
+            print(node.children.keys())  # Print characters stored at the current node
+            for child in node.children.values():
+                self.print_node_contents(child) 
+
+# List of JSON documents similar to the provided structure
+list_of_documents = [
+    {
+        "field": "abcnews--2022-01-01--Colorado fire victims begin new year surveying destruction",
+        "date": "2022-01-01",
+        "source": "abcnews",
+        "title": "Colorado fire victims begin new year surveying destruction",
+        "word_list": ["Hundreds", "Colorado", "residents", "surveying", "more_words"],  # Replace ellipsis with actual words
+        "url": "https://abcnews.go.com/US/wireStory/mississippi-set-execute-man-killing-16-year-girl-95208253",
+        "author": "",
+        "publication_date": "Wed, 14 Dec 2022 01:46:18 -0500"
+    },
+    # Add more documents...
+]
+
+# Initialize Forward Index
+forward_index = ForwardIndex()
+
+# Build Forward Index for each document
+for doc in list_of_documents:
+    hash_value = forward_index.hash_document(doc['title'])
+    word_list = doc['word_list']
+    forward_index.insert_word_list(hash_value, word_list)
+
+# Example usage:
+# Retrieve word list for a specific document
+document_title = "Colorado fire victims begin new year surveying destruction"
+word_list_pointer = forward_index.get_word_list(document_title)
+
+if word_list_pointer:
+    print(f"Word list for '{document_title}': {word_list_pointer}")
+else:
+    print(f"Document '{document_title}' not found in the index.")
+    

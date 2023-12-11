bb529fa09a0a55e8e5f6df823b8d94d04629bbd7
EggsyOnCode <farzansaqib2003@gmail.com>
2023-12-12 00:23:16 +0500

    updated code for forward index

--- indexing/main.py
+++ indexing/main.py
@@ -13,13 +13,13 @@ forward_index = ForwardIndex()
 
 # file path var
 # update as per requirement
-file_path_input = parent_directory + '/test_data/tokenTest.json'
+file_path_input = parent_directory + '/test_data/output1.json'
 file_path_output = parent_directory + '/indexing/forward_index/new_index.json'
 
-# forward_index.genIndex(file_path_input)
-# forward_index.serialize_index();
-# forward_index.deserialize_index_from_json(file_path_output)
-# print(forward_index.get_word_list("Video: Zoo tiger shot while biting man's arm as he screams"))
+forward_index.genIndex(file_path_input)
+forward_index.serialize_index();
+forward_index.deserialize_index_from_json(file_path_output)
+print(forward_index.get_word_list("Video: Zoo tiger shot while biting man's arm as he screams"))
 
 
 # reversed_index = ReversedIndex()
